e4e75cb5d0cdc4d28edbb70e642f53d0614a694e
Leandro Araújo <169188222+euleoaraujo@users.noreply.github.com>
2025-09-04 10:37:23 -0300

    site completo

---
+++ damara-raina/main.js
@@ -0,0 +1,32 @@
+window.revelar = ScrollReveal({ reset: true });
+
+revelar.reveal('.txt', {
+    duration: 2000,
+    distance: '90px',
+});
+revelar.reveal('.b-t-1', {
+    duration: 2000,
+    distance: '90px',
+})
+revelar.reveal('.b-t-2', {
+    duration: 3000,
+    distance: '90px',
+})
+revelar.reveal('.b-t-3', {
+    duration: 4000,
+    distance: '90px',
+})
+revelar.reveal('.txt-r', {
+    duration: 2000,
+    distance: '40px',
+    origin: "right",
+})
+revelar.reveal('.img-l', {
+    duration: 2000,
+    distance: '40px',
+    origin: "left",
+})
+revelar.reveal('.bottom',{
+    duration:2500,
+    distance: '20px',
+})
